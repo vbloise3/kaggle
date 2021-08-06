3ea8145fcbbc223e67abd5aaf86c020ffecacdbf
Vince Bloise <vbloise3@gmail.com>
2021-08-06 14:20:24 -0400

    corrected merged linked list

--- Merge-two-sorted-linked-lists.py
+++ Merge-two-sorted-linked-lists.py
@@ -14,7 +14,7 @@ class LinkedList:
     self.head = None
 
   def getdata(self):
-      return self.data
+      return self.head.data
 
   def insertAtEnd(self,data):
         temp=Node(data)
@@ -38,52 +38,60 @@ class LinkedList:
 
 # Helper function
 def getData(node):
-    return node.data            
+    return node.data  
+
+def printLL(list):
+  node = list.head
+  while node:
+    print(node.data, ' ')
+    node = node.next
 
 # merge sorted linked lists 
-def merge_sorted(head1, head2):
-  # if both lists are empty then merged list is also empty
-  # if one of the lists is empty then other is the merged list
-  if head1 == None:
-    return head2
-  elif head2 == None:
-    return head1
-
-  mergedHead = None
-  if head1.data <= head2.data:
-    mergedHead = head1
-    head1 = head1.next
-  else:
-    mergedHead = head2
-    head2 = head2.next
-
-  mergedTail = mergedHead
-  
-  while head1 != None and head2 != None:
-    temp = None
-    if head1.data <= head2.data:
-      temp = head1
-      head1 = head1.next
+def mergelinkedlists(l_1, l_2):
+    LL1 = LinkedList()
+    LL2 = LinkedList()
+    for i in l_1:
+        LL1.insertAtEnd(i)
+    for i in l_2:
+        LL2.insertAtEnd(i)
+    # if both lists are empty then merged list is also empty
+    # if one of the lists is empty then other is the merged list
+    if LL1.head == None:
+        return LL2
+    elif LL2.head == None:
+        return LL1
+    mergedTail = LinkedList()
+    if LL1.head.data <= LL2.head.data:
+        mergedTail.insertAtEnd(LL1.head.data)
+        LL1.head = LL1.head.next
     else:
-      temp = head2
-      head2 = head2.next
-
-    mergedTail.next = temp
-    mergedTail = temp
-
-  if head1 != None:
-    mergedTail.next = head1
-  elif head2 != None:
-    mergedTail.next = head2
-
-  return mergedHead
+        mergedTail.insertAtEnd(LL2.head.data)
+        LL2.head = LL2.head.next
+    while LL1.head != None and LL2.head != None:
+        temp = None
+        if getData(LL1.head) <= getData(LL2.head):
+            temp = LL1.head
+            LL1.head = LL1.head.next
+        else:
+            temp = LL2.head
+            LL2.head = LL2.head.next
+        mergedTail.insertAtEnd(temp.data)
+        #mergedTail = temp
+    if LL1.head != None:
+        mergedTail.insertAtEnd(LL1.head.data)
+    elif LL2 != None:
+        mergedTail.insertAtEnd(LL2.head.data)
+    return mergedTail
     
 if __name__ == '__main__':
     # map(str, range(number + 1))
     # command line input: "10,11,12,13,14,15,18,23" "2,3,4,5,6,7,19,20"
     firstlist = sys.argv[1]
+    #firstlist = "10,11,12,13,14,15,18,23"
     secondlist = sys.argv[2]
-    l_1 = list(map(int, firstlist.split(',')))
-    l_2 = list(map(int, secondlist.split(',')))
-    # print(l_1)
-    print(merge_sorted([4, 8, 15, 19, 22],[7, 9, 10, 16]))
+    #secondlist = "2,3,4,5,6,7,19,20"
+    l_1 = map(int, firstlist.split(','))
+    l_2 = map(int, secondlist.split(','))
+    # print(inputnumbers)
+    theMergedList = mergelinkedlists(l_1, l_2)
+    theMergedList.traverse_list()
