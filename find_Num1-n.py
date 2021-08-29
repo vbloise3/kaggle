878daafea00ff0bbcd8147e0620f99d6c7390273
Vince Bloise <vbloise3@gmail.com>
2021-08-29 06:43:29 -0400

    made find missing number use a class

--- find_Num1-n.py
+++ find_Num1-n.py
@@ -1,19 +1,24 @@
-# find missing number in an array of ints from 1-n
 import sys
-
-def find_missing(theList):
-    found = set()
-    _size = len(theList)
-    for item in theList:
-        found.add(item)
-    j = 1
-    while j <= _size:
-        if j not in found:
-            return j
-        j += 1
-    return -1
+class Finder(object):
+    def __init__(self, theList = [1,2,4,5,6,7,8,9]):
+        self.theList = theList
+    def find_missing_number(self):
+        _size = len(self.theList)
+        found = set()
+        for item in self.theList:
+            found.add(item)
+        j = 1
+        while j <= _size:
+            if j not in found:
+                return j
+            j += 1
+        return -1
 
 if __name__ == "__main__":
-    arguments = sys.argv[1]
-    input = list(map(int, arguments.split(",")))
-    print(find_missing(input))
+    if len(sys.argv) > 1:
+        arguments = sys.argv
+        input = list(map(int, arguments[1].split(",")))
+        my_finder = Finder(input)
+    else:
+        my_finder = Finder()
+    print(my_finder.find_missing_number())
