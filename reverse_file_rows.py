d99baa5b6688ba5b443d94659a91c3cd9b5185e1
Vince Bloise <vbloise3@gmail.com>
2021-10-07 16:31:56 -0400

    added Kaggle notebook for Micro Emini S&P 500 futures

--- reverse_file_rows.py
+++ reverse_file_rows.py
@@ -1,5 +1,5 @@
-read_file_path = "micro_emini_nasdaq_historical.csv"
-write_file_path = "reversed_micro_emini_nasdaq_historical.csv"
+read_file_path = "micro_emini_sp_500.csv"
+write_file_path = "reversed_micro_emini_sp_500_historical.csv"
 with open(read_file_path) as data_file:
     rows_read = [row.rstrip() for row in data_file]
 print(len(rows_read))
